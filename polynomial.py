3affe5cf59f1d08782068eeca111d6251ad383bc
Haixi Zhang <haixi23@bu.edu>
2024-01-24 18:45:03 -0500

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -34,6 +34,41 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+class Sub:
+    def __init__(self, p1, p2):
+        self.p1 = p1  
+        self.p2 = p2  
+
+    def __repr__(self):
+        if isinstance(self.p2, (Add, Sub)):
+            return repr(self.p1) + " - (" + repr(self.p2) + ")"
+        return repr(self.p1) + " - " + repr(self.p2)
+
+class Div:
+    def __init__(self, p1, p2):
+        self.p1 = p1  
+        self.p2 = p2  
+
+    def __repr__(self):
+        numerator = repr(self.p1)
+        denominator = repr(self.p2)
+        
+        if isinstance(self.p1, (Add, Sub)):
+            numerator = "( " + numerator + " )"
+        if isinstance(self.p2, (Add, Sub)):
+            denominator = "( " + denominator + " )"
+        
+        return numerator + " / " + denominator
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+
+# Example usage:
+p1 = Add(X(), Int(2)) 
+p2 = Mul(X(), Int(3))  
+
+sub_expr = Sub(p1, p2)  
+div_expr = Div(p1, p2)  
+
+print(sub_expr)  
+print(div_expr) 
